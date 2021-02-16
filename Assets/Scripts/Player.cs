2aa895215d76d6b174e2a9bd70ec91e5af9f1582
balabin Anton <aa7224@gmail.com>
2021-02-16 21:34:16 +0200

    Added movement by right click

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,31 +2,29 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private NavMeshAgent agent;
+
     public void OnMouse(InputValue context)
     {
-        Debug.Log("Invoked Mouse");
-        Debug.Log(context);
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        
+        if (Physics.Raycast(ray, out hit)) {
+            Transform objectHit = hit.transform;
+
+            agent.SetDestination(hit.point);
+            Debug.Log(hit.point);
+        }
     }
-    
-    public void OnMouse(InputAction.CallbackContext context)
-    {
-        Debug.Log("Invoked Mouse");
-        Debug.Log(context);
-    }
-    
+
     public void OnKeyboard(InputValue context)
     {
-        Debug.Log("Invoked Mouse");
-        Debug.Log(context);
-    }
-    
-    public void OnKeyboard(InputAction.CallbackContext context)
-    {
-        Debug.Log("Invoked Mouse");
+        Debug.Log("Invoked Keyboard");
         Debug.Log(context);
     }
 }
